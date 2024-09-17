49dd797b367846be59bf4b4cb49698a1c22ea423
221910310008 <221910310008@gitam.in>
2024-09-17 12:57:51 -0400

    BrowserRouter Change

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-// import React from "react";
-// import HomePage from "./Pages/HomePage/HomePage";
-// import PersonalInfo from "./Pages/HomePage/PersonalInfo";
-// import { Routes, Route } from "react-router-dom";
-// import StudentServices from "./Pages/HomePage/StudentServices";
-// import EmployeeServices from "./Pages/HomePage/EmpServices";
-// import FinancialAid from "./Pages/HomePage/FinancialAid";
-// import Header from "./components/header";
-// import Registration from "./Pages/StudentServices/Registration";
-// import RegPlan from "./Pages/Registration/RegPlan";
-// import StudentRegisteration from "./Pages/Registration/StudentRegisteration";
-// import PersonalProfile from "./Pages/PersonalInformation/PersonalProfile";
-// import StudentProfile from "./Pages/StudentServices/StudentProfile";
-// import Help from "./Pages/help";
-// import EmployeeDashboard from "./Pages/Employee Services/EmployeeDashboard";
-// import EmployeeTimesheet from "./Pages/Employee Services/EmployeeTimesheet";
-// import FinancialDashboard from "./Pages/FinancialAidServices/FinancialDashboard";
-// import StudentRecords from "./Pages/StudentServices/StudentRecords";
-// import GraduationDateAccordion from "./Pages/StudentServices/GraduationDateAccordion";
+import React from "react";
+import HomePage from "./Pages/HomePage/HomePage";
+import PersonalInfo from "./Pages/HomePage/PersonalInfo";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentServices from "./Pages/HomePage/StudentServices";
+import EmployeeServices from "./Pages/HomePage/EmpServices";
+import FinancialAid from "./Pages/HomePage/FinancialAid";
+import Header from "./components/header";
+import Registration from "./Pages/StudentServices/Registration";
+import RegPlan from "./Pages/Registration/RegPlan";
+import StudentRegisteration from "./Pages/Registration/StudentRegisteration";
+import PersonalProfile from "./Pages/PersonalInformation/PersonalProfile";
+import StudentProfile from "./Pages/StudentServices/StudentProfile";
+import Help from "./Pages/help";
+import EmployeeDashboard from "./Pages/Employee Services/EmployeeDashboard";
+import EmployeeTimesheet from "./Pages/Employee Services/EmployeeTimesheet";
+import FinancialDashboard from "./Pages/FinancialAidServices/FinancialDashboard";
+import StudentRecords from "./Pages/StudentServices/StudentRecords";
+import GraduationDateAccordion from "./Pages/StudentServices/GraduationDateAccordion";
 
-// // import FinancialInfo from "./Pages/Financial Aid/FinancialInfo";
+// import FinancialInfo from "./Pages/Financial Aid/FinancialInfo";
 
 
-// export default function App() {
-//   return (
-//     <div>
-//       {/* <div style={{ backgroundColor: '#4CAF50', minHeight: '100vh' }}> Set background color and full height */}
+export default function App() {
+  return (
+    <div>
+      {/* <div style={{ backgroundColor: '#4CAF50', minHeight: '100vh' }}> Set background color and full height */}
 
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/personalInfo" element={<PersonalInfo />} />
-//         <Route path="/studentServices" element={<StudentServices />} />
-//         <Route path="/employeeServices" element={<EmployeeServices />} />
-//         <Route path="/financialAid" element={<FinancialAid />} />
-//         <Route path="/registration" element={<Registration />} />
-//         <Route path="/regplan" element={<RegPlan />} />
-//         <Route path="/studentregistration" element={<StudentRegisteration />} />
-//         <Route path="/personalprofile" element={<PersonalProfile />} />
-//         <Route path="/studentprofile" element={<StudentProfile />} />
-//         <Route path="/help" element={<Help />} />
-//         <Route path="/employeedashboard" element={<EmployeeDashboard />} />
-//         <Route path="/employeetimesheet" element={<EmployeeTimesheet />} />
-//         <Route path="/studentrecords" element={<StudentRecords />} />
-//         <Route path="/financialAidDashboard" element={<FinancialDashboard />} />
-//         <Route path="/graduationdateaccordion" element={<GraduationDateAccordion />} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/personalInfo" element={<PersonalInfo />} />
+        <Route path="/studentServices" element={<StudentServices />} />
+        <Route path="/employeeServices" element={<EmployeeServices />} />
+        <Route path="/financialAid" element={<FinancialAid />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/regplan" element={<RegPlan />} />
+        <Route path="/studentregistration" element={<StudentRegisteration />} />
+        <Route path="/personalprofile" element={<PersonalProfile />} />
+        <Route path="/studentprofile" element={<StudentProfile />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/employeedashboard" element={<EmployeeDashboard />} />
+        <Route path="/employeetimesheet" element={<EmployeeTimesheet />} />
+        <Route path="/studentrecords" element={<StudentRecords />} />
+        <Route path="/financialAidDashboard" element={<FinancialDashboard />} />
+        <Route path="/graduationdateaccordion" element={<GraduationDateAccordion />} />
 
-//       </Routes >
-//     </div >
-//     // </div >
-//   )
-// }
+      </Routes >
+    </div >
+    // </div >
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage/HomePage";
-import PersonalInfo from "./Pages/HomePage/PersonalInfo";
-import StudentServices from "./Pages/HomePage/StudentServices";
-import EmployeeServices from "./Pages/HomePage/EmpServices";
-import FinancialAid from "./Pages/HomePage/FinancialAid";
-import Header from "./components/header";
-import Registration from "./Pages/StudentServices/Registration";
-import RegPlan from "./Pages/Registration/RegPlan";
-import StudentRegisteration from "./Pages/Registration/StudentRegisteration";
-import PersonalProfile from "./Pages/PersonalInformation/PersonalProfile";
-import StudentProfile from "./Pages/StudentServices/StudentProfile";
-import Help from "./Pages/help";
-import EmployeeDashboard from "./Pages/Employee Services/EmployeeDashboard";
-import EmployeeTimesheet from "./Pages/Employee Services/EmployeeTimesheet";
-import FinancialDashboard from "./Pages/FinancialAidServices/FinancialDashboard";
-import StudentRecords from "./Pages/StudentServices/StudentRecords";
-import GraduationDateAccordion from "./Pages/StudentServices/GraduationDateAccordion";
-import "./index.css"; // Import global styles if needed
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/personalInfo" element={<PersonalInfo />} />
-          <Route path="/studentServices" element={<StudentServices />} />
-          <Route path="/employeeServices" element={<EmployeeServices />} />
-          <Route path="/financialAid" element={<FinancialAid />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/regplan" element={<RegPlan />} />
-          <Route path="/studentregistration" element={<StudentRegisteration />} />
-          <Route path="/personalprofile" element={<PersonalProfile />} />
-          <Route path="/studentprofile" element={<StudentProfile />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/employeedashboard" element={<EmployeeDashboard />} />
-          <Route path="/employeetimesheet" element={<EmployeeTimesheet />} />
-          <Route path="/studentrecords" element={<StudentRecords />} />
-          <Route path="/financialAidDashboard" element={<FinancialDashboard />} />
-          <Route path="/graduationdateaccordion" element={<GraduationDateAccordion />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
